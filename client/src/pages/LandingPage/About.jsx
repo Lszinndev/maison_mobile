510b77086b988b4f61bfab92e75b1637a12efbc4
Alaertes <alaertes.junior@gmail.com>
2026-05-28 22:59:10 -0300

    MAISON fix: ajuste geral de bug no front

--- client/src/pages/LandingPage/About.jsx
+++ client/src/pages/LandingPage/About.jsx
@@ -116,8 +116,7 @@ export default function About() {
           ))}
         </div>
 
-        {/* Bottom Gradient Overlay to transition to Cases (bg-neutral-950) */}
-        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-b from-transparent to-neutral-950 pointer-events-none z-20" />
+
       </div>
 
       {/* Right Column: Netflix-style Diagonal Photo Marquee */}
